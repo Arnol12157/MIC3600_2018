5079e5dbb27072ed67a86416cb6ce4c6002c6c3d
arnolrt <arnol12157@gmail.com>
2018-08-20 16:37:16 -0400

    added some art and better scene

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    public Vector3 target;
+    private NavMeshAgent agent;
+    public Boolean bGoToTarget;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bGoToTarget)
+        {
+            agent.SetDestination(target);
+            bGoToTarget = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        
+        bGoToTarget = true;
+    }
+}
---
+++ Assets/Scripts/GestureController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using GestureRecognizer;
+using UnityEngine.UI;
+
+public class GestureController : MonoBehaviour {
+
+    public Text messageArea;
+     
+    void OnEnable() {
+        GestureBehaviour.OnRecognition += OnGestureRecognition;
+    }
+
+    void OnDisable() {
+        GestureBehaviour.OnRecognition -= OnGestureRecognition;
+    }
+
+    void OnDestroy() {
+        GestureBehaviour.OnRecognition -= OnGestureRecognition;
+    }
+
+    /// <summary>
+    /// The method to be called on recognition event
+    /// </summary>
+    /// <param name="r">Recognition result</param>
+    void OnGestureRecognition(Result r) {
+        SetNumber(r.Name);
+    }
+
+    /// <summary>
+    /// Shows the number
+    /// </summary>
+    /// <param name="text">Text to show</param>
+    public void SetNumber(string text) {
+        messageArea.text = text;
+    }
+}
